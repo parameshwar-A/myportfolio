b58849419b917234150ca5866f8f3e115c883d4b
Parameshwar Arunachalam <parameshwar273@gmail.com>
2026-07-18 08:14:15 +0530

    Changed the config for github pages

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ export const CONFIG = {
   // Site Settings
   // ---------------------------------------------------------------------------
   site: {
-    url: "https://alexmercer.dev",
+    url: "https://parameshwar-A.github.io",
     locale: "en_US",
     twitterHandle: "@alexmercer_dev",
   },
